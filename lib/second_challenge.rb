f19cbdaf9bd29cb6d22fd5595a5df96a92245d79
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-04 17:35:53 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,18 +2,16 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-        :patriach => {},
+        :patriarch => {},
         :matriarch => {},
         :hero => {},
         :hero_friends => []
       },
     :capulet => {
       #your key/value pairs here!
-        :patriach => {},
+        :patriarch => {},
         :matriarch => {},
         :heroine => {},
         :heroine_friends => []
       }}
-
-
 end
