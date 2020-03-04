570d9082b896b87c58db0b99650a2fdf1f09e27e
Jason Nordheim <jason.nordheim@gmail.com>
2020-03-04 17:33:56 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -10,12 +10,12 @@ def fourth_challenge
         :name => "Steven",
         :age => "30",
         :attitude => "confused"
-      },
-      {
-        :name => "Nurse",
-        :age => "44",
-        :attitude => "worried"
-      }
+        },
+        {
+          :name => "Nurse",
+          :age => "44",
+          :attitude => "worried"
+        }
       ]
    },
    :capulet => {
